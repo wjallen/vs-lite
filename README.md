4430cdecec52c39aa5998ceb03a923016deaa502
William Allen <wallen@tacc.utexas.edu>
2018-03-21 11:34:27 -0500

    testing things out

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-initial commit
+### VS-Lite
+
+#### Requirements
+
+1. UCSF DOCK 6.8
+2. AmberTools 17
+3. UCSF Chimera
+
+#### Prep in Chimera
+
+1. Open protein / ligand in Chimera
+2. Add Hs and charge protein with recent AmberFF
+3. Add Hs and charge ligand with Gasteiger
+4. Save protein only as mol2
+5. Save ligands only as mol2
+6. Delete Hs from protein and save again as pdb
+
+#### Files
+
+1. Name ligand as XXX.mol2 and put in `~/zzz.master` directory
+2. Name receptor as XXX.mol2 and put in `~/zzz.master` directory
+3. Name protein as XXX.noH.pdb and put in `~/zzz.master` directory
+
+
---
+++ run.001.lig_clean_am1bcc.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# This script prepares the ligand that will be used as a footprint reference.
+# Input required is:
+#
+#     ${SYSTEM}.lig.chimera.mol2
+#
+# which is the ligand prepared in chimera by adding hydrogens and computing
+# gasteiger charges.
+#
+
+### Set some paths
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/run.vars.sh"
+
+
+### Check to see if the ligfile exists
+if [ ! -e ${MASTERDIR}/${SYSTEM}.lig.chimera.mol2 ]; then
+	echo "Ligand file does not seem to exist. Exiting.";
+	exit
+fi
+
+
+### Make the lig-prep directory
+rm -fr ${ROOTDIR}/${SYSTEM}/001.lig-prep/
+mkdir -p ${ROOTDIR}/${SYSTEM}/001.lig-prep/
+cd ${ROOTDIR}/${SYSTEM}/001.lig-prep/
+
+##################################################
+cat <<EOF >dock.lig.in
+ligand_atom_file                     temp.mol2
+limit_max_ligands                    no
+skip_molecule                        no
+read_mol_solvation                   no
+calculate_rmsd                       no
+use_database_filter                  no
+orient_ligand                        no
+use_internal_energy                  no
+flexible_ligand                      no
+bump_filter                          no
+score_molecules                      no
+ligand_outfile_prefix                lig
+write_orientations                   no
+num_scored_conformers                1
+rank_ligands                         no
+EOF
+##################################################
+
+### Pre-process the ligand with DOCK
+#echo -n "$SYSTEM LIG : "
+perl -pe 's/\r\n/\n/g' ${MASTERDIR}/${SYSTEM}.lig.chimera.mol2 > temp.mol2
+#perl ${SCRIPTDIR}/lig_unique_name.pl temp1.mol2 ${SYSTEM} yes > temp2.mol2
+ln -s ${TACC_DOCK_PARAM}/flex.defn ./
+ln -s ${TACC_DOCK_PARAM}/flex_drive.tbl ./
+ln -s ${TACC_DOCK_PARAM}/vdw_AMBER_parm99.defn ./vdw.defn
+${DOCKDIR}/dock6 -i dock.lig.in -o dock.lig.out
+mv lig_scored.mol2 ${SYSTEM}.lig.processed.mol2
+#rm -f temp1.mol2 temp2.mol2 lig_scored.mol2 dock.lig.in
+
+
+
+exit
+
+### Compute ligand charges with antechamber
+${AMBERDIR}/acdoctor -i ${SYSTEM}.lig.processed.mol2 -f mol2
+#${AMBERDIR}/antechamber -fi mol2 -fo mol2 -c bcc -j 5 -at sybyl -s 2 -pf y \
+#                        -i ${SYSTEM}.lig.processed.mol2 -o ${SYSTEM}.lig.am1bcc.mol2
+${AMBERDIR}/antechamber -fi mol2 -fo mol2 -c gas -j 4 -at sybyl -s 2 -pf y \
+                        -i ${SYSTEM}.lig.processed.mol2 -o ${SYSTEM}.lig.am1bcc.mol2
+
+exit
+
+if [ `grep "No convergence in SCF" sqm.out | wc -l` ]; then
+	${AMBERDIR}/antechamber -fi mol2 -fo mol2 -c bcc -j 5 -at sybyl -s 2 -pf y \
+                                -ek "itrmax=100000, qm_theory='AM1', grms_tol=0.0002, tight_p_conv=0, scfconv=1.d-8" \
+                                -i $SYSTEM.lig.processed.mol2 -o $SYSTEM.lig.am1bcc.mol2
+fi
+mv sqm.out sqm.lig.out
+mv sqm.in sqm.lig.in
+
+
